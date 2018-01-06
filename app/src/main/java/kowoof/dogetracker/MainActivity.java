0d651a3dba3420886ba28be2396383a479020897
Marcin Popko <marcinpopko@outlook.com>
2018-01-06 14:07:56 +0100

    UI project to get sure what I want to do.

--- app/src/main/java/kowoof/dogetracker/MainActivity.java
+++ app/src/main/java/kowoof/dogetracker/MainActivity.java
@@ -22,15 +22,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setLogo(R.mipmap.main_icon);
+        getSupportActionBar().setTitle(" DogeTracker");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -87,10 +80,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
