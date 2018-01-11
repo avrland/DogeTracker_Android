43ed8138d762647e7952f81f8eee270f66233bfa
Marcin Popko <marcinpopko@outlook.com>
2018-01-11 22:29:31 +0100

    Added exchange rate refreshing and showing this stuff into main view

--- app/src/main/java/kowoof/dogetracker/MainActivity.java
+++ app/src/main/java/kowoof/dogetracker/MainActivity.java
@@ -1,11 +1,15 @@
 package kowoof.dogetracker;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,34 +21,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    ProgressDialog dialog;
+    doge_rates current_doge_rates = new doge_rates();
+    Handler myHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setLogo(R.mipmap.main_icon);
-        getSupportActionBar().setTitle(" DogeTracker");
+        getSupportActionBar().setTitle("DogeTracker");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        myHandler = new Handler();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+//        refresh_rates(); //TODO AUTO REFRESH with every startup
     }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -102,8 +112,38 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    public void refresh_rates(View view) {
+    public void refresh_button(View view) {
+            refresh_rates();
+    }
+    public void refresh_rates(){
+         //Getting current date and time
+        DateFormat df = new SimpleDateFormat("d MMM yyyy, HH:mm:ss");
+        String date = df.format(Calendar.getInstance().getTime());
+        //Getting current dogecoin rates from coinmarketcap
+        current_doge_rates.get_rates(this);
+//        if(current_doge_rates.doge_rate == null){
+//            current_doge_rates.get_rates(this);
+//            current_doge_rates.get_rates(this);
+//        }
 
+        //Finding textView items from layout
+        TextView doge_rates_text = (TextView)findViewById(R.id.doge_rate);
+        TextView hour_change_text = (TextView)findViewById(R.id.hour_change);
+        TextView daily_change_text = (TextView)findViewById(R.id.daily_change);
+        TextView weekly_change_text = (TextView)findViewById(R.id.weekly_change);
+        TextView market_cap_text = (TextView)findViewById(R.id.market_cap);
+        TextView volume_text = (TextView)findViewById(R.id.volume);
+        TextView total_supply_text = (TextView)findViewById(R.id.total_supply);
+        TextView last_update_text = (TextView)findViewById(R.id.last_update);
+
+        doge_rates_text.setText("1Đ = " + current_doge_rates.doge_rate + "$");
+        hour_change_text.setText("1h: " + current_doge_rates.hour_change + "%");
+        daily_change_text.setText("24h: " + current_doge_rates.daily_change + "%");
+        weekly_change_text.setText("7d: " + current_doge_rates.weekly_change + "%");
+        market_cap_text.setText("Market cap: " + current_doge_rates.market_cap + "$");
+        volume_text.setText("Volume 24h: " + current_doge_rates.volume + "$");
+        total_supply_text.setText("Total supply: " + current_doge_rates.total_supply + "Đ");
+        last_update_text.setText("Last update: " + date);
     }
     //toast function to get it a little bit shorter
     public void make_toast(String messege_toast){
--- app/src/main/java/kowoof/dogetracker/doge_rates.java
+++ app/src/main/java/kowoof/dogetracker/doge_rates.java
@@ -1,5 +1,29 @@
 package kowoof.dogetracker;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
 /**
  * Created by Marcin on 10.01.2018.
  */
@@ -7,6 +31,48 @@ package kowoof.dogetracker;
 public class doge_rates {
         String doge_rate, hour_change, daily_change, weekly_change, market_cap, volume,
                 total_supply;
+        ProgressDialog dialog;
+        String url = "https://api.coinmarketcap.com/v1/ticker/dogecoin/";
 
+        public void get_rates(final Context current_context){
+            dialog = new ProgressDialog(current_context);
+            dialog.setMessage("Loading....");
+            dialog.show();
+
+            StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String string) {
+                    parseJsonData(string);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Toast.makeText(current_context, "Some error occurred!!", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
+            RequestQueue rQueue = Volley.newRequestQueue(current_context);
+            rQueue.add(request);
+            dialog.dismiss();
+        }
+    void parseJsonData(String jsonString) {
+        try {
+            JSONArray jsonarray = new JSONArray(jsonString);
+            for(int i=0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                doge_rate       = jsonobject.getString("price_usd");
+                hour_change     = jsonobject.getString("percent_change_1h");
+                daily_change    = jsonobject.getString("percent_change_24h");
+                weekly_change   = jsonobject.getString("percent_change_7d");
+                market_cap      = jsonobject.getString("market_cap_usd");
+                volume          = jsonobject.getString("24h_volume_usd");
+                total_supply    = jsonobject.getString("total_supply");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        dialog.dismiss();
+    }
 
 }
