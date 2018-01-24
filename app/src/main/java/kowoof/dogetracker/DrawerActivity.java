6ec58c1237b5716abc5158cbbd1d55540da3d0d7
Marcin Popko <marcinpopko@outlook.com>
2018-01-24 19:23:24 +0100

    Added the same navigation view in MainActivity and wallet_list view, working good

---
+++ app/src/main/java/kowoof/dogetracker/DrawerActivity.java
@@ -0,0 +1,116 @@
+package kowoof.dogetracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class DrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    protected RelativeLayout fullLayout;
+    protected FrameLayout frameLayout;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        //Prepare default drawer view section
+        fullLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.activity_drawer, null);
+        frameLayout = (FrameLayout) fullLayout.findViewById(R.id.drawer_frame);
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(fullLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        toolbar.setTitle("DogeTracker");
+        toolbar.setSubtitle("v1.0");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //End of preparing default drawer view section
+    }
+
+
+    //We close drawer here with pressing back button
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //Opening drawer here
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //Options from right toolbar, I don't use it now (maybe someday)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Hamburger menu selection segment
+    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(getApplicationContext(), wallet_list.class);
+            startActivity(i);
+            finish();
+        } else if (id == R.id.nav_gallery) {
+//            make_toast("Not implemented yet.");
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+            finish();
+        } else if (id == R.id.nav_slideshow) { //redirect to dogecin reddit
+            Uri uri = Uri.parse("https://www.reddit.com/r/dogecoin/");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+            make_toast("Not implemented yet.");
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //toast function to get it a little bit shorter
+    public void make_toast(String messege_toast){
+        Context context = getApplicationContext();
+        CharSequence text = messege_toast;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
--- app/src/main/java/kowoof/dogetracker/MainActivity.java
+++ app/src/main/java/kowoof/dogetracker/MainActivity.java
@@ -1,47 +1,24 @@
 package kowoof.dogetracker;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends DrawerActivity {
 
     //We create doge_rates object and handler to make getting exchange rates wow
     doge_rates current_doge_rates;
     Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Prepare main view section
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("DogeTracker");
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        //End of preparing main view section
 
         //We create handler to wait for get exchange rates
         handler = new Handler(){
@@ -60,66 +37,7 @@ public class MainActivity extends AppCompatActivity
         refresh_rates();
     }
 
-    //We close drawer here with pressing back button
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
-    //Opening drawer here
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    //Options from right toolbar, I don't use it now (maybe someday)
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    //Hamburger menu selection segment
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            Intent i = new Intent(getApplicationContext(), wallet_list.class);
-            startActivity(i);
-            //finish();
-        } else if (id == R.id.nav_gallery) {
-            make_toast("Not implemented yet.");
-        } else if (id == R.id.nav_slideshow) { //redirect to dogecin reddit
-            Uri uri = Uri.parse("https://www.reddit.com/r/dogecoin/");
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
-        } else if (id == R.id.nav_manage) {
-            make_toast("Not implemented yet.");
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
     //Refresh button - selects response for clicking refresh - refresh_rates
     //Refresh rates - calling get_rates from doge_rates class
@@ -172,4 +90,7 @@ public class MainActivity extends AppCompatActivity
         toast.show();
     }
 
+    public void TEST(View view) {
+        make_toast("TEST");
+    }
 }
