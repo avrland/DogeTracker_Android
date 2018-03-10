8b888b735cbbbe6a459f2966227bcba8ae269288
avrland <marcinpopko@outlook.com>
2018-03-10 11:34:51 +0100

    Added splash screen (we'll find out if it'll we useful)

---
+++ app/src/main/java/kowoof/dogetracker/Splash.java
@@ -0,0 +1,36 @@
+package kowoof.dogetracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+    Animation anim;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView=(ImageView)findViewById(R.id.imageView2); // Declare an imageView to show the animation.
+                anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
+                anim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+        imageView.startAnimation(anim);
+    }
+}
